50dfbb5634359d8e399a5b7675ab3da6be99420c
Claus Gumbmann <claus.gumbmann@schule.bayern.de>
2024-09-23 07:53:07 +0200

    Vorbereitung

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,31 +7,5 @@ public class Main {
 
   public static void main(String[] args) {
 
-    MyClass[] myclassArray = new MyClass[100];
-
-    for (int i = 0; i < myclassArray.length; i++) {
-      myclassArray[i] = new MyClass(new Random().nextInt(1000) + 1);
-    }
-
-    System.out.println(Arrays.deepToString(myclassArray));
-    Arrays.sort(myclassArray);
-    System.out.println(Arrays.deepToString(myclassArray));
-
-    int counter = 0;
-    for (int i = 0; i < myclassArray.length; i++) {
-      if (myclassArray[i].getObjNumber() % 2 == 0) {
-        counter++;
-      }
-    }
-
-    MyClass[] geradeObjNummer = new MyClass[counter];
-
-    counter = 0;
-    for (int i = 0; i < myclassArray.length; i++) {
-      if (myclassArray[i].getObjNumber() % 2 == 0) {
-        geradeObjNummer[counter++] = myclassArray[i];
-      }
-    }
-    System.out.println(Arrays.deepToString(geradeObjNummer));
   }
 }
--- src/main/java/classes/MyClass.java
+++ src/main/java/classes/MyClass.java
@@ -1,6 +1,6 @@
 package classes;
 
-public class MyClass implements Comparable<MyClass> {
+public class MyClass {
 
     public static int COUNTER;
     private final int objNumber;
@@ -8,20 +8,5 @@ public class MyClass implements Comparable<MyClass> {
     public MyClass(int objNumber) {
         this.objNumber = objNumber;
         MyClass.COUNTER++;
-    }
-
-    // nötig für Entscheidung gerade oder ungerade
-    public int getObjNumber() {
-        return objNumber;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("Ich bin Objekt Nummer: %d", objNumber);
-    }
-
-    @Override
-    public int compareTo(MyClass other) {
-        return this.objNumber - other.objNumber;
     }
 }
